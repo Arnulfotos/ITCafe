cf9b285b334d2431c50d5d6bdd5190d66bcef4ca
arnulfotos <manuelleon1996@gmail.com>
2024-06-16 16:27:28 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿
+
+
+using System;
+using System.IO;
+using System.Threading.Channels;
+//try
+//{
+ConsoleKeyInfo cki;
+var menu = new List<string[]>();
+var comanda = new List<string[]>();
+
+do
+{
+
+   
+    // Ruta del archivo
+    string filePath = "c:/ITCafe/menu_Itstep_cafeteria.txt";
+
+    // Lee todas las líneas del archivo y las carga en un array de strings
+    string[] lines = File.ReadAllLines(filePath);
+
+    // Imprime el contenido del array
+    /* foreach (string line in lines)
+     {
+         Console.WriteLine(line);
+     } */
+    int counter = 1;
+    foreach (string line in lines)
+    {
+        var item = line.Split(',');
+        menu.Add(item);
+    }
+
+
+    Console.WriteLine(String.Format("| {0,-3} {1,-18} {2,-60} {3,-5} |", "", "", "Menu", ""));
+    Console.WriteLine("-----------------------------------------------------------------------------------------");
+    foreach (string[] product in menu)
+    {
+        Console.WriteLine(String.Format("| {0,-5} | {1,-18} | {2,-60} | {3,-5} |", counter, $"{product[0]}", product[1], product[2]));
+        counter++;
+    }
+
+ 
+    bool pedidoFinalizado = false;
+    while (!pedidoFinalizado) { 
+        Console.WriteLine("Eliga el item que desee agregar");
+        int tempItem = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"Usted eligio {menu[tempItem - 1][0]}\n");
+
+        Console.WriteLine("Eliga la cantidad a desear");
+        int tempQuant = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < tempQuant; i++)
+        {
+            comanda.Add(menu[tempItem - 1]);
+        }
+
+        Console.WriteLine("Desea agregar otra cosa? Presione cualquier tecla para continuar");
+        Console.WriteLine("Si no desea agregar mas, presione N");
+
+        string eleccion = Console.ReadLine()!;
+       
+
+        if (eleccion.ToLower() == "n")
+        {
+            pedidoFinalizado = true;
+        } 
+    }
+
+
+    int total = 0;
+  
+
+    foreach (string[] elemento in comanda)
+    {
+        Console.WriteLine(elemento[0]);
+        total += int.Parse(elemento[2].Substring(1));
+    }
+    Console.WriteLine(total);
+
+
+
+
+
+    Console.WriteLine("\nPresione cualquier tecla si desea continuar");
+    Console.WriteLine("Presione ESC si desea salir...");
+
+    cki = Console.ReadKey();
+    Console.Clear();
+} while (cki.Key != ConsoleKey.Escape);
+
+
+
+/*}
+catch (Exception e)
+{
+    Console.WriteLine("Ocurrió un error: " + e.Message);
+} */
+
+
+
